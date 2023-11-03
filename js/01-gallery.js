a43a9812e4f2754fc0dc15a1d5a81c2f26a42f75
VladAkolzin <akolzin95@gmail.com>
2023-11-03 20:19:06 +0100

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const list = document.querySelector(".gallery");
+list.insertAdjacentHTML("beforeend", createMarkUp(galleryItems));
+list.addEventListener("click", handleClick);
+function handleClick(event) {
+  event.preventDefault();
+  if (event.target === event.currentTarget) {
+    return;
+  }
+  const modalImg = event.target.dataset.source;
+  const instance = basicLightbox.create(`
+    <img src="${modalImg}" width="800" height="600">
+`);
 
+  instance.show();
+}
+function createMarkUp(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+  					<a class="gallery__link" href="${original}">
+    					<img
+      				class="gallery__image"
+      				src="${preview}"
+      				data-source="${original}"
+     				 alt="${description}"
+    				/>
+  				</a>
+				</li>`
+    )
+    .join("");
+}
 console.log(galleryItems);
